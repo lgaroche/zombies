fc7b3b6012caefe31d8eaecbbad7fcd4964b55cf
Louis Garoche <louis@garoche.me>
2022-10-30 18:44:58 +0100

    material ui and app bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useMemo } from "react"
+import useMediaQuery from "@mui/material/useMediaQuery"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  )
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
