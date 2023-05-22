eff644f74329fa9b3ca28f11ca78beb22ac3ccd4
Louis <louis@garoche.me>
2023-05-23 00:02:00 +0200

    better tests

--- tests/deployAndRegister.ts
+++ tests/deployAndRegister.ts
@@ -1,4 +1,9 @@
-import { get_account, set_endpoint, set_quiet } from "@completium/experiment-ts"
+import {
+  get_account,
+  set_endpoint,
+  set_quiet,
+  expect_to_fail,
+} from "@completium/experiment-ts"
 import { Tzombies, add_for_all } from "./bindings/tzombies"
 import { Permits } from "./bindings/permits"
 import { Market } from "./bindings/market"
@@ -8,7 +13,6 @@ import { ledger_key } from "./bindings/tzombies"
 const assert = require("assert")
 
 set_quiet(true)
-set_endpoint("mockup")
 
 let permits: Permits = new Permits()
 let market: Market = new Market()
@@ -20,14 +24,17 @@ const bob = get_account("bob")
 describe("Contracts deployment", async () => {
   it("permits", async () => {
     await permits.deploy(alice.get_address(), { as: alice })
+    console.log("permits", permits.address)
   })
 
   it("market", async () => {
     await market.deploy({ as: alice })
+    console.log("market", market.address)
   })
 
   it("fa2", async () => {
     await fa2.deploy(alice.get_address(), permits.get_address(), { as: alice })
+    console.log("fa2", fa2.address)
   })
 })
 
@@ -60,40 +67,65 @@ describe("Register NFTs", async () => {
       { as: alice }
     )
   })
-  it("get config", async () => {
-    console.log("market", market.address)
-    console.log("fa2", fa2.address)
-    console.log("permits", permits.address)
-  })
 })
 
 describe("Mint and trade", async () => {
   it("mint zombie", async () => {
-    await fa2.mint(alice.get_address(), new Nat(1), new Nat(1), {
-      as: alice,
-      amount: new Tez(2),
-    })
+    await fa2.mint(
+      alice.get_address(), // tow
+      new Nat(1), // tid
+      new Nat(1), // nbt
+      {
+        as: alice,
+        amount: new Tez(2),
+      }
+    )
     // check that Alice now has 1 zombie
     const key = new ledger_key(alice.get_address(), new Nat(1))
     const amount = await fa2.get_ledger_value(key)
     assert(amount?.to_number() === 1)
   })
-  it("approve marketplace", async () => {
-    const arg = new add_for_all(market.get_address())
-    await fa2.update_operators_for_all([arg], { as: alice })
-  })
+
   it("sell zombie", async () => {
     await market.sell(
       fa2.get_address(),
-      new Nat(1),
-      new Nat(1),
-      new Tez(5),
-      new Date("2023-12-01T00:00:00Z"),
+      new Nat(1), // token_id_
+      new Nat(1), // amount_
+      new Tez(5), // price_
+      new Date("2023-12-01T00:00:00Z"), // expiry_
       {
         as: alice,
       }
     )
   })
+
+  it("buy zombie before operator update should fail", async () => {
+    await expect_to_fail(async () => {
+      await market.buy(new Nat(1), new Nat(1), { amount: new Tez(5), as: bob })
+    }, fa2.errors.INVALID_CALLER)
+
+    // check that Alice still has 1 zombie
+    const key = new ledger_key(alice.get_address(), new Nat(1))
+    const amount = await fa2.get_ledger_value(key)
+    assert(amount?.to_number() === 1)
+  })
+
+  it("approve marketplace", async () => {
+    const arg = new add_for_all(market.get_address())
+    await fa2.update_operators_for_all([arg], { as: alice })
+  })
+
+  it("buy zombie without enough tez should fail", async () => {
+    await expect_to_fail(async () => {
+      await market.buy(new Nat(1), new Nat(1), { amount: new Tez(1), as: bob })
+    }, market.errors.r_value)
+
+    // check that Alice still has 1 zombie
+    const key = new ledger_key(alice.get_address(), new Nat(1))
+    const amount = await fa2.get_ledger_value(key)
+    assert(amount?.to_number() === 1)
+  })
+
   it("buy zombie", async () => {
     await market.buy(new Nat(1), new Nat(1), { amount: new Tez(5), as: bob })
 
